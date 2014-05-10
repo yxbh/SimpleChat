9166b187abd726e5c5e392f05efd2934b60fa87a
yxbh <y.x.ben.h+github@gmail.com>
2014-05-10 15:20:16 +0800

    added routing for chat system.

--- Src/merge/config/routes.rb
+++ Src/merge/config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   match '/signin',	to: 'sessions#new',				via: 'get'
   match '/signout',	to: 'sessions#destroy',		via: 'delete'
 
+  get 'chats/room'
+  get 'chats/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
