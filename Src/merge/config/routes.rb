60716abdbf08f9323f19215defcbf4fbb66678ce
yxbh <y.x.ben.h+github@gmail.com>
2014-05-11 12:12:47 +0800

    fix whitespace (tab to space)

--- Src/merge/config/routes.rb
+++ Src/merge/config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   root  'static_pages#home'
-  match '/help',		to: 'static_pages#help',		via: 'get'
-  match '/about',	to: 'static_pages#about',	via: 'get'
-  match '/contact',	to: 'static_pages#contact',	via: 'get'
-  match '/signup',	to: 'users#new',					via: 'get'
-  match '/signin',	to: 'sessions#new',				via: 'get'
-  match '/signout',	to: 'sessions#destroy',		via: 'delete'
+  match '/help',      to: 'static_pages#help',      via: 'get'
+  match '/about',     to: 'static_pages#about',     via: 'get'
+  match '/contact',   to: 'static_pages#contact',   via: 'get'
+  match '/signup',    to: 'users#new',              via: 'get'
+  match '/signin',    to: 'sessions#new',           via: 'get'
+  match '/signout',   to: 'sessions#destroy',       via: 'delete'
   
-  match '/create', 	to: 'chats#create', 				via: 'get'
-  match '/room', 	to: 'chats#room',				via: 'get'
+  match '/create',    to: 'chats#create',           via: 'get'
+  match '/room',      to: 'chats#room',             via: 'get'
 
   #get 'chats/room'
   #get 'chats/create'
