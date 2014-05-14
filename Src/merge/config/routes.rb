7840b29484658dc80fff84630e4b592af85a0eae
yxbh <y.x.ben.h+github@gmail.com>
2014-05-15 01:02:45 +0800

    new route for user page

--- Src/merge/config/routes.rb
+++ Src/merge/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   match '/about',     to: 'static_pages#about',     via: 'get'
   match '/contact',   to: 'static_pages#contact',   via: 'get'
   match '/signup',    to: 'users#new',              via: 'get'
+  match '/user',      to: 'users#show',             via: 'get'
   match '/signin',    to: 'sessions#new',           via: 'get'
   match '/signout',   to: 'sessions#destroy',       via: 'delete'
   
