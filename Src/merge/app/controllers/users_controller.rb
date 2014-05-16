d28a7251d047c01eaa88fbd471427912c224923a
Tim Lander <tim57282@hotmail.com>
2014-05-16 14:06:37 +0800

    Fixed typo

--- Src/merge/app/controllers/users_controller.rb
+++ Src/merge/app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample Chat!"
+      flash[:success] = "Welcome to Simple Chat!"
       redirect_to @user
     else
       render 'new'
