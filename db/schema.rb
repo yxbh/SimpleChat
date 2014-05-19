280a4c6b139bdcde12adf8cd05a96ddae1cdda29
Tim Lander <tim57282@hotmail.com>
2014-05-19 16:33:13 +0800

    Fixed navbar overlap with narrow window

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,6 @@ ActiveRecord::Schema.define(version: 20140517031505) do
     t.datetime "updated_at"
   end
 
-  create_table "images", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
