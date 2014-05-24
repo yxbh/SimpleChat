102c53e4a25cc4f5e5ac496ad52cb86d26a09d5f
yxbh <y.x.ben.h+github@gmail.com>
2014-05-24 11:04:58 +0800

    fix whitespace and indentation

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -8,12 +8,12 @@ def full_title(page_title)
 end
 
 def gotoChatPage()
-	let(:user) { FactoryGirl.create(:user) }
-	before do
-		visit signin_path
-		fill_in "Email",			with: user.email.upcase
-		fill_in "Password",	with: user.password
-		click_button "Sign In"
-		click_link "Click here to start a new room"
-	end
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    visit signin_path
+    fill_in       "Email",    with: user.email.upcase
+    fill_in       "Password", with: user.password
+    click_button  "Sign In"
+    click_link    "Click here to start a new room"
+  end
 end
