5a86a0c6837d24e5cc458f02741eb86d5a508e88
yxbh <y.x.ben.h+github@gmail.com>
2014-05-24 10:58:57 +0800

    indentation fix

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,16 +29,16 @@ describe "Authentication" do
   describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",      with: user.email.upcase
+        fill_in "Email",    with: user.email.upcase
         fill_in "Password", with: user.password
         click_button "Sign In"
       end
 
       it { should have_content(user.name) }
       it { should have_title(user.name) }
-      it { should have_link('new room', href: create_path) }
-      it { should have_link('Sign out',   href: signout_path) }
-      it { should_not have_link('Sign In',  href: signin_path) }
+      it { should have_link('new room',    href: create_path) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign In', href: signin_path) }
     
     describe "followed by signout" do
         before { click_link "Sign out" }
