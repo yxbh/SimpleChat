933758f5d0ba4dae54c652178b3797fc670df785
Tim Lander <tim57282@hotmail.com>
2014-05-24 18:06:08 +0800

    Fix pathing issue on windows

--- spec/requests/upload_pages_spec.rb
+++ spec/requests/upload_pages_spec.rb
@@ -6,13 +6,14 @@ describe "Upload pages" do
 
   describe "Upload", :js => true do
     gotoChatPage()
-    file_path = Rails.root.join('spec', 'fixtures', 'files', 'test-upload.txt')
+    file_path = Rails.root.join('spec', 'fixtures', 'files', 'test-upload.txt').to_s.gsub('/', '\\')
 
     before do
       attach_file "file", file_path
       click_button "Upload"
     end
-
+	
+    sleep(1)
     it { should have_content('Download') }
   end
 end
