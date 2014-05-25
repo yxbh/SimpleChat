ed8fa3885ba6a575f32af3a633384057d7793d7c
Ben Huang <y.x.ben.h+github@gmail.com>
2014-05-25 13:28:10 +0800

    experimental port switch

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+require 'rails/commands/server'
+
+module DefaultOptions
+  def default_options
+    super.merge!(Port: 3051)
+  end
+end
+
+Rails::Server.send(:prepend, DefaultOptions)
